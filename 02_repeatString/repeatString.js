2696e233894507d07f7250a8f2ef5e27c815fcc0
Zendard <thijsmuylaert@hotmail.com>
2023-06-30 23:07:47 +0200

    Finished 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function (string, num) {
+	returnString = "";
+	if (num >= 0) {
+		for (i = 0; i < num; i++) {
+			returnString += string;
+		}
+	} else {
+		returnString = "ERROR";
+	}
+	return returnString;
 };
 
 // Do not edit below this line

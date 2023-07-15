31b307683d3bae56dbf2a3988d1e58a35ec5ea74
Zendard <thijsmuylaert@hotmail.com>
2023-07-15 13:57:30 +0200

    Finish 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,9 @@
-const sumAll = function() {
-
+const sumAll = function (startInt, endInt) {
+	let sum = 0;
+	for (i = startInt; i <= endInt; i++) {
+		sum += i;
+	}
+	return sum;
 };
 
 // Do not edit below this line

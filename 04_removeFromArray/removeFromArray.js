ea4b95e2a05d83bd261a88d0c930d467bb6dc3d1
Zendard <thijsmuylaert@hotmail.com>
2023-07-15 13:23:03 +0200

    Finished 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (array) {
+	for (i = 1; i < arguments.length - 1; i++) {
+		const index = array.indexOf(arguments[i]);
+		array.splice(index, 1);
+	}
+	return array;
 };
 
 // Do not edit below this line

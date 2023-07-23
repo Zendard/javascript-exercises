ac9cef8e646fbd0cbd5b035bb1dc0b5a89968312
Zendard <thijsmuylaert@hotmail.com>
2023-07-23 19:59:26 +0200

    Finish 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,33 +1,45 @@
-const add = function() {
-	
+const add = function (firstNumber, secondNumber) {
+	return firstNumber + secondNumber;
 };
 
-const subtract = function() {
-	
+const subtract = function (firstNumber, secondNumber) {
+	return firstNumber - secondNumber;
 };
 
-const sum = function() {
-	
+const sum = function (inputArray) {
+	let sum = 0;
+	for (i = 0; i < inputArray.length; i++) {
+		sum += inputArray[i];
+	}
+	return sum;
 };
 
-const multiply = function() {
-
+const multiply = function () {
+	let product = 1;
+	for (i = 0; i < arguments.length; i++) {
+		product *= arguments[i];
+	}
+	return product;
 };
 
-const power = function() {
-	
+const power = function (number, exponent) {
+	return number ** exponent;
 };
 
-const factorial = function() {
-	
+const factorial = function (number) {
+	let factorial = 1;
+	for (i = 1; i <= number; i++) {
+		factorial *= i;
+	}
+	return factorial;
 };
 
 // Do not edit below this line
 module.exports = {
-  add,
-  subtract,
-  sum,
-  multiply,
-  power,
-  factorial
+	add,
+	subtract,
+	sum,
+	multiply,
+	power,
+	factorial,
 };

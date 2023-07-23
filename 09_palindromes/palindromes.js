ec0de4ab51a02123d0bd6759dd57b648c1d2ece0
Zendard <thijsmuylaert@hotmail.com>
2023-07-23 21:01:04 +0200

    Finish 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,27 @@
 const palindromes = function () {
-
+	let trueCount = 0;
+	let wordCount = 0;
+	for (i = 0; i < arguments.length; i++) {
+		let word = arguments[i];
+		word = word.replace(/!/g, "");
+		word = word.replace(/\./g, "");
+		word = word.replace(/,/g, "");
+		word = word.replace(/ /g, "");
+		word = word.toLowerCase();
+		let reverseWord = "";
+		for (letter = word.length - 1; letter >= 0; letter--) {
+			reverseWord += word[letter];
+		}
+		if (word == reverseWord) {
+			trueCount++;
+		}
+		wordCount++;
+	}
+	if (trueCount >= wordCount) {
+		return true;
+	} else {
+		return false;
+	}
 };
 
 // Do not edit below this line
